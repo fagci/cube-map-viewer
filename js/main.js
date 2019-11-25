ac012425adc19b15f8fb0a0e4b9e9de555b2a6d4
Mikhail Yudin <yumih@r-o.ru>
2019-11-25 19:54:38 +0700

    navigation with constraints

---
+++ js/main.js
@@ -0,0 +1,206 @@
+/**
+ * @file Shows Cube Maps to users as Google Map Street View
+ * @copyright Mikhail Yudin aka fagci
+ * @author fagci / https://github.com/fagci https://mikhail-yudin.ru
+ */
+
+const quality = window.location.search.match(/quality=(\d+)/)[1] || 256
+
+console.info('Quality set to', quality)
+
+const ROOM_SIZE = 3.0 // Размер комнаты
+const SRC_PATH = '/res/br' + (+quality) + '/' // Папка с рендерами кубов
+const FILE_POINTS = '/res/point.txt' // Файл с точками и путями
+const FOV = 70 // Угол обзора камеры
+
+const CUBES_ROTATION = Math.PI / 2 // коррекция поворота кубов
+const IS_INSIDEOUT = false // Вывернутый наизнанку рендер
+
+const HALF_RS = ROOM_SIZE / 2
+
+let container
+let camera, scene, renderer
+let minimapCamera
+let stats
+
+let controls
+let navigation
+
+let cubesGroup
+
+let windowHalfX, windowHalfY
+let insetWidth, insetHeight
+
+const debugPane = document.querySelector('#debugpane')
+const cubeGeometry = new CubeGeometry(ROOM_SIZE)
+const textureManager = new TextureManager(SRC_PATH, '#loading', '.progressbar')
+
+let overlapFixHeight = 0.0
+
+function makeRoomFromSS (index, position, neighbourPoints) {
+  let fName = 'big_render' + ('000' + index).slice(-4) + '.jpg'
+  const map = textureManager.load(fName)
+  map.minFilter = map.magFilter = THREE.LinearFilter
+  map.anisotropy = renderer.capabilities.getMaxAnisotropy()
+  const mat = new THREE.MeshBasicMaterial({ map, side: THREE.BackSide, color: 0xffffff })
+  const cube = new THREE.Mesh(cubeGeometry, mat)
+  if (IS_INSIDEOUT) cube.scale.x = -1
+  cube.rotation.y = CUBES_ROTATION
+
+  cube.neighbourPoints = []
+  for (let np of neighbourPoints) cube.neighbourPoints.push('Cube_' + np)
+
+  cube.position.copy(position)
+  cube.layers.disable(0)
+  cube.layers.enable(1)
+  cube.name = 'Cube_' + index
+
+  cube.position.y += overlapFixHeight
+  overlapFixHeight += 0.0001
+
+  console.log(cube)
+
+  cubesGroup.add(cube)
+}
+
+function parsePoints (t) {
+  t.split('\n').forEach((m) => {
+    let matches = m.match(/^(\d+) X=([0-9.-]+) Y=([0-9.-]+) Z=([0-9.-]+) ([\d,]+)/i)
+    if (!matches) return
+
+    const renderNumber = matches[1]
+
+    const x = +matches[2]
+    const y = +matches[3]
+    const z = +matches[4]
+
+    const neighbourPoints = matches[5].split(',')
+
+    const cubePosition = new THREE.Vector3(x, y, z)
+    // const pointHelper = new THREE.AxesHelper(0.5)
+    //
+    // pointHelper.position.copy(cubePosition)
+    // pointHelper.layers.enable(1)
+    // pointHelper.material.depthTest = false
+    // pointHelper.material.transparent = true
+    // scene.add(pointHelper)
+    makeRoomFromSS(renderNumber, cubePosition, neighbourPoints)
+  })
+  navigation.setRoom(cubesGroup.children[0]) // Первая комната
+}
+
+function initRooms () {
+  cubesGroup = new THREE.Group()
+  cubesGroup.name = 'Cubes group'
+  scene.add(cubesGroup)
+
+  // const gridHelper = new THREE.GridHelper(10, 10)
+  // const axisHelper = new THREE.AxesHelper(5)
+  //
+  // gridHelper.layers.enable(1)
+  // axisHelper.layers.enable(1)
+  //
+  // gridHelper.material.depthTest = false
+  // axisHelper.material.depthTest = false
+  //
+  // gridHelper.material.transparent = true
+  // axisHelper.material.transparent = true
+  //
+  // scene.add(gridHelper)
+  // scene.add(axisHelper)
+
+  fetch(FILE_POINTS).then((d) => d.text()).then(parsePoints)
+}
+
+function initCamera () {
+  camera = new THREE.PerspectiveCamera(FOV, window.innerWidth / window.innerHeight, 0.01, 200)
+  camera.position.set(0, 0, -0.01)
+
+  camera.lookAt(200, HALF_RS, 0)
+}
+
+function initMinimap () {
+  let mmSize = 6 // TODO: make it relative to full map size
+  minimapCamera = new THREE.OrthographicCamera(-mmSize, mmSize, mmSize, -mmSize, 0.01, 1000)
+  scene.add(minimapCamera)
+
+  minimapCamera.position.y = 20
+  minimapCamera.lookAt(camera.position)
+  minimapCamera.layers.enable(1)
+  minimapCamera.layers.disable(0)
+}
+
+function initScene () {
+  scene = new THREE.Scene()
+  window.scene = scene
+
+  initRooms()
+  initCamera()
+  initMinimap()
+
+  navigation = new Navigation(scene, cubesGroup, camera, minimapCamera)
+}
+
+function initRenderer () {
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  container.appendChild(renderer.domElement)
+}
+
+function init () {
+  THREE.Cache.enabled = true
+  stats = new Stats()
+  debugPane.appendChild(stats.domElement)
+  container = document.createElement('div')
+  document.body.appendChild(container)
+
+  initRenderer()
+  initScene()
+
+  window.addEventListener('resize', onWindowResize, false)
+  onWindowResize()
+
+  controls = new PanControls(camera, container, mouse => navigation.handleClick(mouse))
+}
+
+function onWindowResize () {
+  windowHalfX = window.innerWidth / 2
+  windowHalfY = window.innerHeight / 2
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  insetWidth = window.innerHeight / 4
+  insetHeight = window.innerHeight / 4
+
+  minimapCamera.aspect = insetWidth / insetHeight
+  minimapCamera.updateProjectionMatrix()
+}
+
+function animate () {
+  stats.begin()
+  TWEEN.update()
+  requestAnimationFrame(animate)
+
+  navigation.update(controls.mouse)
+
+  render()
+  stats.end()
+}
+
+function render () {
+  renderer.setClearColor(0xffffff)
+  renderer.setViewport(0, 0, window.innerWidth, window.innerHeight)
+  renderer.render(scene, camera)
+
+  renderer.setClearColor(0x333333)
+  renderer.clearDepth()
+  renderer.setScissorTest(true)
+  renderer.setScissor(16, 16, insetWidth, insetHeight)
+  renderer.setViewport(16, 16, insetWidth, insetHeight)
+  renderer.render(scene, minimapCamera)
+  renderer.setScissorTest(false)
+}
+
+init()
+animate()
---
+++ js/navigation.js
@@ -0,0 +1,211 @@
+/**
+ * @file Helps with navigation and movement with animation
+ * @copyright Mikhail Yudin aka fagci
+ * @author fagci / https://github.com/fagci https://mikhail-yudin.ru
+ */
+
+class Navigation {
+  constructor (scene, rooms, camera, minimapCamera) {
+    this.rooms = rooms
+    this.camera = camera
+    this.scene = scene
+    this.minimapCamera = minimapCamera
+    this.isMoving = false
+
+    this.raycaster = new THREE.Raycaster()
+
+    this.navHelper = new THREE.Mesh(
+      new THREE.SphereGeometry(0.1, 32, 32),
+      new THREE.MeshBasicMaterial({ color: 0x00ff00, depthTest: false, transparent: true })
+    )
+    this.navHelper.layers.enable(1)
+    this.navHelper.layers.enable(0)
+    // this.navHelper.renderOrder = 1
+
+    this.whereAmI = new THREE.Mesh(
+      new THREE.SphereGeometry(0.45, 16, 16),
+      new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    )
+
+    this.whereAmI.layers.disable(0)
+    this.whereAmI.layers.enable(1)
+    this.whereAmI.position.y = 1
+
+    this.arrowHelper = new THREE.ArrowHelper(
+      // first argument is the direction
+      new THREE.Vector3(0, 0, 0),
+      // second argument is the orgin
+      new THREE.Vector3(0, 0, 0),
+      // length
+      2,
+      // color
+      0xff0000, 0.5, 0.5)
+
+    this.arrowHelper.line.layers.disable(0)
+    this.arrowHelper.line.layers.enable(1)
+    this.arrowHelper.cone.layers.disable(0)
+    this.arrowHelper.cone.layers.enable(1)
+
+    this.whereAmI.add(this.arrowHelper)
+
+    scene.add(this.whereAmI)
+    scene.add(this.navHelper)
+
+    window.addEventListener('keyup', e => this.onKeyUp(e))
+  }
+
+  setRoom (room) {
+    if (this.currentRoom) this.currentRoom.layers.disable(0)
+    this.currentRoom = room
+    this.currentRoom.layers.enable(0)
+    this.camera.position.copy(this.currentRoom.position)
+    this.whereAmI.position.copy(this.currentRoom.position)
+  }
+
+  update (mouse) {
+    this.raycaster.setFromCamera(mouse, this.camera)
+    const farPoint = this.getFarIntersectionPoint()
+    if (!farPoint) {
+      this.navHelper.visible = false
+      return
+    }
+
+    this.navHelper.position.copy(farPoint)
+    this.navHelper.visible = true
+
+    let lookAtVector = new THREE.Vector3(0, 0, -1)
+    lookAtVector.applyQuaternion(camera.quaternion)
+    this.arrowHelper.setDirection(lookAtVector)
+
+  }
+
+  getFarIntersectionPoint () {
+    let maxDistancePoint
+    const otherRooms = this.rooms.children.filter((ch) => {
+      return ch.object !== this.currentRoom
+    })
+
+    let intersections = this.raycaster.intersectObjects(otherRooms)
+    if (!intersections.length) return null
+
+    let maxDistance = 0
+
+    intersections.filter((ch) => {
+      const distance = ch.point.distanceTo(this.camera.position)
+      if (maxDistance < distance) {
+        maxDistancePoint = ch.point
+        maxDistance = distance
+      }
+    })
+    return maxDistancePoint
+  }
+
+  getNearestIntersectionObject (lookAtVector) {
+    // get lines between potential movements
+    // get angle between camera direction and each line
+    // get minimal angle
+    // move to direction with this line
+
+    lookAtVector.y = 0
+
+    const currentRoom = this.currentRoom
+    let targetRoom
+    let minimumAngle = Number.MAX_VALUE
+
+    let dir = new THREE.Vector3() // create once an reuse it
+
+    for (let np of currentRoom.neighbourPoints) {
+      let neighbourRoom = this.rooms.getObjectByName(np)
+
+      let p1 = currentRoom.position.clone()
+      let p2 = neighbourRoom.position.clone()
+
+      let directionVector = dir.subVectors(p2, p1).normalize()
+      let angle = Math.abs(lookAtVector.angleTo(directionVector))
+      console.log(angle)
+      if (angle < minimumAngle && angle < Math.PI * FOV / 360.0) {
+        minimumAngle = angle
+        targetRoom = neighbourRoom
+      }
+    }
+    return targetRoom
+  }
+
+  handleClick () {
+    this.moveByLookAtVector()
+  }
+
+  moveByLookAtVector (lookAtVector) {
+    if (this.isMoving) return
+    if (!lookAtVector) {
+      lookAtVector = new THREE.Vector3(0, 0, -1)
+      lookAtVector.applyQuaternion(camera.quaternion)
+    }
+    let dstCube = this.getNearestIntersectionObject(lookAtVector)
+    if (dstCube) this.moveToCube(dstCube)
+  }
+
+  moveToCube (dstCube) {
+    if (this.currentRoom === dstCube) return
+    this.isMoving = true
+
+    const srcCube = this.currentRoom
+    srcCube.layers.enable(0)
+    dstCube.layers.enable(0)
+
+    dstCube.material.opacity = 1
+    srcCube.material.transparent = true
+    dstCube.material.transparent = false
+
+    animateVector3(this.camera.position, dstCube.position, {
+      duration: 1000,
+      easing: TWEEN.Easing.Cubic.InOut,
+      update: (d) => {
+        srcCube.material.opacity = 1 - TWEEN.Easing.Cubic.In(d)
+        this.whereAmI.position.copy(this.camera.position)
+      },
+      callback: () => {
+        srcCube.layers.disable(0)
+        srcCube.material.opacity = 1
+        srcCube.material.transparent = false
+
+        this.currentRoom = dstCube
+        this.whereAmI.position.copy(this.currentRoom.position)
+        console.warn('Current cube:', this.currentRoom.position)
+        this.isMoving = false
+      }
+    })
+  }
+
+  onKeyUp (e) { // TODO: перенести в контролы
+    let lookAtVector = new THREE.Vector3(0, 0, -1)
+    lookAtVector.applyQuaternion(camera.quaternion)
+    const axis = new THREE.Vector3(0, 1, 0)
+
+    switch (e.which) {
+      case 87:
+      case 38:
+        // forward
+        this.moveByLookAtVector(lookAtVector)
+        break
+      case 83:
+      case 40:
+        // backward
+        lookAtVector.applyAxisAngle(axis, Math.PI)
+        this.moveByLookAtVector(lookAtVector)
+        break
+      case 65:
+      case 37:
+        // left
+        lookAtVector.applyAxisAngle(axis, Math.PI / 2)
+        this.moveByLookAtVector(lookAtVector)
+        break
+      case 68:
+      case 39:
+        // right
+        lookAtVector.applyAxisAngle(axis, -Math.PI / 2)
+        this.moveByLookAtVector(lookAtVector)
+        break
+    }
+  }
+}
